9f35c2eb27ee6052ed50ebf1a5cef2a55944339e
Manish Kumar <mk778281@gmail.com>
2020-09-13 23:38:00 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-center text-primary'>Twitter</h1>
+    </div>
+  );
+}
+
+export default App;
