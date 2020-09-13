952b2471b7ac8cf0b1672c8f458544472cd79f82
Manish Kumar <mk778281@gmail.com>
2020-09-13 23:55:23 +0530

    image selected

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     <h1 className='text-center text-primary'>Twitter</h1>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1 className='text-center text-primary'>Twitter</h1>
+			<div className='landing-image bg'>
+				<img src='/images/image.png' alt='main' />
+			</div>
+		</div>
+	);
 }
 
 export default App;
